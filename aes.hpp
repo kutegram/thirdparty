ba82986dde973a6a16655eb4c3c4d1019e490c6f
curoviyxru <me@curoviyx.ru>
2020-11-06 13:34:59 +0300

    Symbian fix

--- aes.hpp
+++ aes.hpp
@@ -1,9 +1,9 @@
 #ifndef _AES_HPP_
 #define _AES_HPP_
 
-#ifndef __cplusplus
-#error Do not include the hpp header in a c project!
-#endif //__cplusplus
+//#ifndef __cplusplus
+//#error Do not include the hpp header in a c project!
+//#endif //__cplusplus
 
 extern "C" {
 #include "aes.h"
